493ef56d1c125f1aa0fc80b33da1110374dc1931
Jonathan Zarate <zaratedev@gmail.com>
2018-07-04 18:45:24 -0500

    A Thread can have replies. Feacture for users to read forum threads.

---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $thread;
+    public function setUp() {
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();
+    }
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    function a_user_ca_read_a_single_thread() {
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    function a_user_can_read_replies_that_are_associated_whit_a_thread() {
+
+        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->body);
+    }
+}
