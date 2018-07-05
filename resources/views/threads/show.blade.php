7f1c8b1d398d45a8828a5fe8b5f2870ad5461c9f
Jonathan Zarate <zaratedev@gmail.com>
2018-07-05 10:58:52 -0500

    Add the from reply for user authenticate.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -22,5 +22,26 @@
                 @endforeach
             </div>
         </div>
+        @if(auth()->check())
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form action="{{ url('/threads/'.$thread->id.'/replies') }}" method="post">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" rows="5" placeholder="Have Something to say?" class="form-control"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="alert alert-info" role="alert">
+                        Please <a href="{{ route('login') }}">sing in</a> to participate in this discussion.
+                    </div>
+                </div>
+            </div>
+        @endif
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads', 'ThreadsController@index')->name('threads');
 Route::get('/threads/{thread}', 'ThreadsController@show');
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
