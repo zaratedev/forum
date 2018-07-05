b06d449e157ac75e18600c20d1342355834395e0
Jonathan Zarate <zaratedev@gmail.com>
2018-07-05 11:42:29 -0500

    A user May publish Threads. A user can read can reply to threads.

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+    /** @test */
+    function an_authenticated_user_can_create_new_forum_thread() {
+        // signed in user
+        $this->actingAs(factory('App\User')->create());
+        // When we hit the endpoint to create a new thread
+        $thread = factory('App\Thread')->make();
+        $this->post('/threads', $thread->toArray());
+        // Then, when we visit the thread page
+        $this->get('/threads/'. $thread->id)
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
