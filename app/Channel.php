1d73f9595ac0ea1ab3a6ebfe8ded6388e9b6a4c0
Jonathan Zarate <zaratedev@gmail.com>
2018-07-05 16:39:41 -0500

    A User can filter Threads By Channel. Create TDD for ReadThreadsTest, ChannelTest

--- app/Channel.php
+++ app/Channel.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
-    //
+    /**
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    /**
+     *
+     */
+    public function threads() {
+        return $this->hasMany(Thread::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::get('threads', 'ThreadsController@index');
 Route::get('threads/create', 'ThreadsController@create');
 Route::get('threads/{channel}/{thread}', 'ThreadsController@show');
 Route::post('threads', 'ThreadsController@store');
-//Route::resource('/threads', 'ThreadsController');
+Route::get('threads/{channel}', 'ThreadsController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
