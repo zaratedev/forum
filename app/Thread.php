4d6e6d8908959f2f0e583f6221babaab6ec7802a
Jonathan Zarate <zaratedev@gmail.com>
2018-07-06 11:12:20 -0500

    Meta DEtails and paginate. Add Details of show Thread and Paginate of replies.

--- app/Thread.php
+++ app/Thread.php
@@ -8,6 +8,15 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    public static function boot()
+    {
+      parent::boot();
+
+      static::addGlobalScope('replyCount', function($builder) {
+        $builder->withCount('replies');
+      });
+    }
+
     public function path() {
         return "/threads/{$this->channel->slug}/{$this->id}";
     }
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <a href="">{{$thread->creator->name}}</a> posted:
@@ -13,35 +13,33 @@
                         {{ $thread->body }}
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                @foreach($thread->replies as $reply)
+                @foreach($replies as $reply)
                     @include('threads.reply')
                 @endforeach
+                {{ $replies->links() }}
+                @if(auth()->check())
+                  <form action="{{ url($thread->path().'/replies') }}" method="post">
+                      {{ csrf_field() }}
+                      <div class="form-group">
+                          <textarea name="body" id="body" rows="5" placeholder="Have Something to say?" class="form-control"></textarea>
+                      </div>
+                      <button type="submit" class="btn btn-primary">Post</button>
+                  </form>
+                @else
+                  <div class="alert alert-info" role="alert">
+                      Please <a href="{{ route('login') }}">sing in</a> to participate in this discussion.
+                  </div>
+                @endif
+            </div>
+            <div class="col-md-4">
+              <div class="panel panel-default">
+                <div class="panel-body">
+                  This thread was publish {{ $thread->created_at->diffForHumans() }} by
+                  <a href="#">{{ $thread->creator->name }}</a>,
+                  and currently has {{ $thread->replies_count }} comments.
+                </div>
+              </div>
             </div>
         </div>
-        @if(auth()->check())
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <form action="{{ url($thread->path().'/replies') }}" method="post">
-                        {{ csrf_field() }}
-                        <div class="form-group">
-                            <textarea name="body" id="body" rows="5" placeholder="Have Something to say?" class="form-control"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Post</button>
-                    </form>
-                </div>
-            </div>
-        @else
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="alert alert-info" role="alert">
-                        Please <a href="{{ route('login') }}">sing in</a> to participate in this discussion.
-                    </div>
-                </div>
-            </div>
-        @endif
     </div>
 @endsection
