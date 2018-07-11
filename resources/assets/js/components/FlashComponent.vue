d8f0303f64ba78e237e2a25f6eb095f03521eea2
Jonathan Zarate <jonathan.zarate@dinkbit.com>
2018-07-11 13:10:24 -0500

    Implement as flash messaging with vue js

---
+++ resources/assets/js/components/FlashComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="alert alert-success alert-flash" role="alert" v-show="show">
+    <strong>Sucess!</strong> {{ body }}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['message'],
+    data () {
+      return {
+        body: "",
+        show: false
+      }
+    },
+    created() {
+      if (this.message) {
+        this.flash(this.message);
+      }
+    },
+    methods: {
+      flash(message) {
+        this.body = this.message;
+        this.show = true;
+
+        this.hide();
+      },
+      hide() {
+        setTimeout( () => {
+          this.show = false;
+        }, 3000);
+      }
+    }
+  }
+</script>
+
+<style>
+  .alert-flash {
+    position: fixed;
+    right: 25px;
+    bottom: 25px;
+  }
+</style>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,8 @@
         @include('layouts.nav')
 
         @yield('content')
+
+        <flash message="{{ session('flash') }}"></flash>
     </div>
 
     <!-- Scripts -->
