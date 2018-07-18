0c0ff3fb6fb7fcec189474031b4db2a81c3a4c55
Jonathan Zarate <zaratedev@gmail.com>
2018-07-18 15:34:58 -0500

    Refactoring Registration controller

--- app/User.php
+++ app/User.php
@@ -51,6 +51,7 @@ class User extends Authenticatable
     public function confirm()
     {
         $this->confirmed = true;
+        $this->confirmation_token = false;
         $this->save();
     }
 
